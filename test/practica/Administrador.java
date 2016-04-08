63ff682a45bd8e7e4a5d3cf3a6d395df35bf53c8
inandu1516 <inandu1516@gmail.com>
2016-04-08 18:57:27 +0200

    DDBC - Classe Criatura

--- test/practica/Administrador.java
+++ test/practica/Administrador.java
@@ -9,18 +9,27 @@ public class Administrador extends Usuari {
 
     public Jugador creaJugador(String nom, String pass){
         Jugador usr = new Jugador(nom, pass);
+        
+        //UPDATE table INSERT INTO Table (usr)...
         return usr;
     }
     
     public void modificarNomJugador(Jugador jugador, String nom){
         jugador.nom = nom;
+        //UPDATE jugadors...
     }
     
     public void modificarPassJugador(Jugador jugador, String pass){
         jugador.pass = pass;
+        //UPDATE jugadors...
     }
     
-    //metodes creacio, modificacio i eliminacio usuaris...
-    //metode de cerca d'usuaris...
+    public void eliminarJugador(Jugador jugador, String pass){
+        //delete user from usuaris where nom = nom;
+    }
+    
+    public void cercarJugador(String nom){
+        //SELECT * FROM jugadors WHERE nom = nom;
+    }
     
 }
---
+++ test/practica/Criatura.java
@@ -0,0 +1,82 @@
+package practica;
+
+public class Criatura {
+    
+    private String nom;
+    private int atac;
+    private int defensa;
+    private String raça;
+    private String medi;
+    private String habilitat_esp;
+    private String imatge; 
+    private boolean ready = false;
+
+    
+    /* ---- Getters & Setters (fer UPDATES a la BD!)*/
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAtac() {
+        return atac;
+    }
+
+    public void setAtac(int atac) {
+        this.atac = atac;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public String getRaça() {
+        return raça;
+    }
+
+    public void setRaça(String raça) {
+        this.raça = raça;
+    }
+
+    public String getMedi() {
+        return medi;
+    }
+
+    public void setMedi(String medi) {
+        this.medi = medi;
+    }
+
+    public String getHabilitat_esp() {
+        return habilitat_esp;
+    }
+
+    public void setHabilitat_esp(String habilitat_esp) {
+        this.habilitat_esp = habilitat_esp;
+    }
+
+    public String getImatge() {
+        return imatge;
+    }
+
+    public void setImatge(String imatge) {
+        this.imatge = imatge;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+    
+    
+    
+}
