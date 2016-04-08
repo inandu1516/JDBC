f98e7a2e2d2d7c78ce2101b402ac181db069fb79
inandu1516 <inandu1516@gmail.com>
2016-04-08 18:57:01 +0200

    DDBC - Classe Criatura

--- test/practica/Jugador.java
+++ test/practica/Jugador.java
@@ -3,14 +3,63 @@ package practica;
 public class Jugador extends Usuari {
     
     protected String lema = null;
-    
+    protected int victories;
+    protected int derrotes;
+    protected int empats;
+
     public Jugador(String nom, String pass) {
         super(nom, pass);
     }
+    
+    public void setVictories(int victories) {
+        this.victories = victories;
+    }
 
+    public int getDerrotes() {
+        return derrotes;
+    }
+
+    public void setDerrotes(int derrotes) {
+        this.derrotes = derrotes;
+    }
+
+    public int getEmpats() {
+        return empats;
+    }
+
+    public void setEmpats(int empats) {
+        this.empats = empats;
+    }
+    
+    /* ---- Getters & Setters (fer UPDATES a la BD!)*/
     public void setLema(String lema) {
         this.lema = lema;
     }
+
+    public String getLema() {
+        return lema;
+    }
+    
+    public int getVictories() {
+        return victories;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
 
     @Override
     public String toString() {
