df096ddc363323148870c93da207cb1898cefa9e
almope1516daw2 <almope1516daw2@gmail.com>
2016-04-15 18:37:44 +0200

    Update jdbc.sql

--- jdbc.sql
+++ jdbc.sql
@@ -7,9 +7,9 @@ USE JDBC;
 CREATE TABLE Jugador (
     nom VARCHAR(20),
     lema VARCHAR(20),
-    victories INT, 
-    derrotes INT, 
-    empats INT,
+    victories INT DEFAULT '0', 
+    derrotes INT DEFAULT '0', 
+    empats INT DEFAULT '0',
     CONSTRAINT PK_Jugador PRIMARY KEY(nom)
 );
 
@@ -33,6 +33,13 @@ CREATE TABLE Criatura (
     CONSTRAINT PK_Criatura PRIMARY KEY(nom),
     CONSTRAINT FK_Criatura_Jugador FOREIGN KEY (propietari) REFERENCES Jugador(nom)
 );
+
+# ------- INSERTS --------
+INSERT INTO Criatures VALUES ('Draco azur', 4, 4, 'Drac', 'aire', null, 'img/draco.jpg','#nomForeignPropietari#');
+INSERT INTO Criatures VALUES ('Asaltante Múrloc', 2, 1, 'Múrloc', 'acuatic', null, 'img/murloc.jpg','#nomForeignPropietari#');
+INSERT INTO Criatures VALUES ('Caballero Múrloc', 3, 4, 'Múrloc', 'acuatic', null, 'img/murloc.jpg','#nomForeignPropietari#');
+INSERT INTO Criatures VALUES ('Rei Mukla', 4, 4, 'Bestia', 'volador', null, 'img/draco.jpg','#nomForeignPropietari#');
+
 
 CREATE TABLE Equip (
     id INT AUTO_INCREMENT,
