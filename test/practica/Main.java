c554ed2ddc87e94b25367b7568a2e660a3f5129b
almope1516daw2 <almope1516daw2@gmail.com>
2016-04-15 15:31:20 +0200

    CreateTables

--- test/practica/Main.java
+++ test/practica/Main.java
@@ -1,9 +1,26 @@
 package practica;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 public class Main {
     
     public static void main(String[] args) {
-        
+           
+        try {
+            crearTaula();
+        } catch (SQLException ex) {
+            for (Throwable t : ex)
+                t.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         /*Administrador Morote = new Administrador();
         
         Jugador Inge = Morote.creaJugador("Inge","nolose");
@@ -13,6 +30,71 @@ public class Main {
         
         System.out.println(Inge + "\n" + Marta);*/
         
+    }
+    
+    public static void crearTaula() throws SQLException, IOException {
+        Connection conn = obtenirConnexio();
+
+        try {
+            Statement stat = conn.createStatement();
+
+             stat.executeUpdate("CREATE TABLE Jugador ("
+                    + "nom VARCHAR(20),"
+                    + "lema VARCHAR(20),"
+                    + "victories INT," 
+                    + "derrotes INT," 
+                    + "empats INT," 
+                    + "PRIMARY KEY(nom))");
+            
+            stat.executeUpdate("CREATE TABLE Criatura ("
+                    + "nom VARCHAR(20),"
+                    + "atac INT,"
+                    + "defensa INT,"
+                    + "raça VARCHAR(10),"
+                    + "medi VARCHAR(10),"
+                    + "habilitat_esp VARCHAR(20),"
+                    + "imatge VARCHAR(20),"
+                    + "propietari VARCHAR(20),"
+                    + "ready BOOLEAN DEFAULT '0',"
+                    + "equip INT,"
+                    + "PRIMARY KEY(nom),"
+                    + "FOREIGN KEY (propietari) REFERENCES Jugador(nom))");
+
+           
+           
+            stat.executeUpdate("CREATE TABLE Equip ("
+                    + "id INT AUTO_INCREMENT,"
+                    + "nomEquip VARCHAR(20),"
+                    + "propietari VARCHAR(20),"
+                    + "ready BOOLEAN DEFAULT '0',"
+                    + "PRIMARY KEY(id),"
+                    + "FOREIGN KEY (propietari) REFERENCES Jugador(nom))");
+
+            
+//            stat.executeUpdate("DROP TABLE Criatura");
+//            stat.executeUpdate("DROP TABLE Jugador");
+//            stat.executeUpdate("DROP TABLE Equip");
+        } finally {
+            conn.close();
+        }
+    }
+
+    
+    public static Connection obtenirConnexio() throws SQLException, IOException {
+        Properties props = new Properties();
+        FileInputStream in = new FileInputStream("/home/randeth/NetBeansProjects/JavaDB/database.properties");
+        props.load(in);
+        in.close();
+
+        String drivers = props.getProperty("jdbc.drivers");
+        if (drivers != null) {
+            System.setProperty("jdbc.drivers", drivers);
+        }
+        String url = props.getProperty("jdbc.url");
+        String usuari = props.getProperty("jdbc.username");
+        String password = props.getProperty("jdbc.password");
+
+        return DriverManager.getConnection(url, usuari, password);
     }
     
 }
