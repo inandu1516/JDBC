5b9e748e62532ebd307f634d925260e4db35fc7a
almope1516daw2 <almope1516daw2@gmail.com>
2016-04-16 16:36:48 +0200

    Update jdbc.sql

--- jdbc.sql
+++ jdbc.sql
@@ -13,7 +13,7 @@ CREATE TABLE Jugador (
     CONSTRAINT PK_Jugador PRIMARY KEY(nom)
 );
 
-# ------- INSERTS --------
+
 INSERT INTO Jugador VALUES ('Ingemar', 'Gogogo', 0, 0, 0);
 INSERT INTO Jugador VALUES ('Marta', 'Em mm emm', 0, 0, 0);
 INSERT INTO Jugador VALUES ('Albert', 'un lema?', 0, 0, 0);
@@ -28,24 +28,24 @@ CREATE TABLE Criatura (
     habilitat_esp VARCHAR(20),
     imatge VARCHAR(20),
     propietari VARCHAR(20),
-    ready BOOLEAN DEFAULT '0',
+    ready BOOLEAN DEFAULT FALSE,
     equip INT,
     CONSTRAINT PK_Criatura PRIMARY KEY(nom),
     CONSTRAINT FK_Criatura_Jugador FOREIGN KEY (propietari) REFERENCES Jugador(nom)
 );
 
-# ------- INSERTS --------
-INSERT INTO Criatures VALUES ('Draco azur', 4, 4, 'Drac', 'aire', null, 'img/draco.jpg','#nomForeignPropietari#');
-INSERT INTO Criatures VALUES ('Asaltante Múrloc', 2, 1, 'Múrloc', 'acuatic', null, 'img/murloc.jpg','#nomForeignPropietari#');
-INSERT INTO Criatures VALUES ('Caballero Múrloc', 3, 4, 'Múrloc', 'acuatic', null, 'img/murloc.jpg','#nomForeignPropietari#');
-INSERT INTO Criatures VALUES ('Rei Mukla', 4, 4, 'Bestia', 'volador', null, 'img/draco.jpg','#nomForeignPropietari#');
+
+INSERT INTO Criatura (nom, atac, defensa, raça, medi, imatge, propietari) VALUES ('Draco azur', 4, 4, 'Dragon', 'aire', 'img/draco.jpg','Albert');
+INSERT INTO Criatura (nom, atac, defensa, raça, medi, imatge, propietari) VALUES ('Asaltante Murloc', 2, 1, 'Murloc', 'acuatico', 'img/murloc.jpg','Albert');
+INSERT INTO Criatura (nom, atac, defensa, raça, medi, imatge, propietari) VALUES ('Caballero Murloc', 3, 4, 'Murloc', 'acuatico', 'img/murloc.jpg','Albert');
+INSERT INTO Criatura (nom, atac, defensa, raça, medi, imatge, propietari) VALUES ('Rei Mukla', 4, 4, 'Bestia', 'volador', 'img/draco.jpg','Albert');
 
 
 CREATE TABLE Equip (
     id INT AUTO_INCREMENT,
     nomEquip VARCHAR(20),
     propietari VARCHAR(20),
-    ready BOOLEAN DEFAULT '0',
+    ready BOOLEAN DEFAULT FALSE,
     CONSTRAINT PK_Equipa PRIMARY KEY(id),
     CONSTRAINT FK_Equip_Jugador FOREIGN KEY (propietari) REFERENCES Jugador(nom)
 );
